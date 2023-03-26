a6cb8d187808cc87bfaffa0df4ff6474ffd4f477
LakshyaSharmaQA <lakshyasharmaqa@gmaiol.com>
2023-03-26 19:14:40 +0530

    new project

--- src/test/java/PackMaven/Demo.java
+++ src/test/java/PackMaven/Demo.java
@@ -2,11 +2,15 @@ package PackMaven;
 
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
 
 public class Demo {
     public static void main(String[] args) throws InterruptedException {
+        ChromeOptions ops;
+        ops = new ChromeOptions();
+        ops.addArguments("--remote-allow-origins=*");
         System.setProperty("webdriver.chrome.driver", "G:/selinium/chromedriver_win32/chromedriver.exe/");
-        WebDriver driver = new ChromeDriver();
+        WebDriver driver = new ChromeDriver(ops);
         driver.manage().window().maximize();
         driver.get("https://www.gooogle.com");
         System.out.println(driver.getTitle());
