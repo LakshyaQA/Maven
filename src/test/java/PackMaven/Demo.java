2eef05dfc8164d00d0913221c22a687c54c3db6d
LakshyaSharmaQA <lakshyasharmaqa@gmaiol.com>
2023-03-26 18:43:04 +0530

    Revert "new project"
    This reverts commit b7769873047f979d190f5bb62739a671a6c46b5c.

--- src/test/java/PackMaven/Demo.java
+++ src/test/java/PackMaven/Demo.java
@@ -2,17 +2,13 @@ package PackMaven;
 
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
-import org.openqa.selenium.chrome.ChromeOptions;
 
 public class Demo {
     public static void main(String[] args) throws InterruptedException {
-        ChromeOptions ops;
-        ops = new ChromeOptions();
-        ops.addArguments("--remote-allow-origins=*");
         System.setProperty("webdriver.chrome.driver", "G:/selinium/chromedriver_win32/chromedriver.exe/");
-        WebDriver driver = new ChromeDriver(ops);
+        WebDriver driver = new ChromeDriver();
         driver.manage().window().maximize();
-        driver.get("https://facebook.com");
+        driver.get("https://www.gooogle.com");
         System.out.println(driver.getTitle());
         System.out.println(driver.getCurrentUrl());
         Thread.sleep(3000);
