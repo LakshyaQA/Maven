7942433a2d0510b7ba231c3a45350ad52edf0b52
LakshyaSharmaQA <lakshyasharmaqa@gmaiol.com>
2023-03-26 18:35:26 +0530

    new project

---
+++ src/test/java/PackMaven/Demo.java
@@ -0,0 +1,18 @@
+package PackMaven;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class Demo {
+    public static void main(String[] args) throws InterruptedException {
+        System.setProperty("webdriver.chrome.driver", "G:/selinium/chromedriver_win32/chromedriver.exe/");
+        WebDriver driver = new ChromeDriver();
+        driver.manage().window().maximize();
+        driver.get("https://www.gooogle.com");
+        System.out.println(driver.getTitle());
+        System.out.println(driver.getCurrentUrl());
+        Thread.sleep(3000);
+        driver.close();
+
+    }
+}
